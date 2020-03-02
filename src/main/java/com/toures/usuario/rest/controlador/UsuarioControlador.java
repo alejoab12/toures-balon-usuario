0db474a29eaad27da2ac309b6cd3fca9343aba84
malcalab <malcalab@everis.com>
2020-03-01 22:45:11 -0500

    Ajusta cors origin

--- src/main/java/com/toures/usuario/rest/controlador/UsuarioControlador.java
+++ src/main/java/com/toures/usuario/rest/controlador/UsuarioControlador.java
@@ -3,10 +3,12 @@ package com.toures.usuario.rest.controlador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,7 @@ import com.toures.usuario.rest.values.UsuarioValue;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(methods = {RequestMethod.GET,RequestMethod.POST},origins = "*")
 public class UsuarioControlador {
 
 	@Autowired

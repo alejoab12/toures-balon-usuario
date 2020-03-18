a149de45adf38fd2c509009e3e73a74d5c32ea15
malcalab <malcalab@everis.com>
2020-03-18 17:49:34 -0500

    remove test

--- src/test/java/com/toures/usuario/UsuarioServicioApplicationTests.java
+++ src/test/java/com/toures/usuario/UsuarioServicioApplicationTests.java
@@ -1,8 +1,5 @@
 package com.toures.usuario;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class UsuarioServicioApplicationTests {
 
